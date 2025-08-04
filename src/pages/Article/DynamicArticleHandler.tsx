72b07625b63fbb7e062366897b290aeca5c32c9c
kuro14122002 <nguyenductoan1412@gmail.com>
2025-08-04 09:38:52 +0700

    initial commit

--- src/pages/Article/DynamicArticleHandler.tsx
+++ src/pages/Article/DynamicArticleHandler.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { useParams, Navigate } from "react-router-dom";
 import { useLanguage } from "@/context/LanguageContext";
 import { useTheme } from "@/context/ThemeContext";
